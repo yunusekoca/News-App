c16224673767561f9918b00780692272ba22de40
Yunus Emre Koca <yekoca@hotmail.com>
2022-08-22 22:54:18 +0300

    Project setup

---
+++ NewsApp/Presentations/News/NewsVC.swift
@@ -0,0 +1,17 @@
+//
+//  NewsViewController.swift
+//  NewsApp
+//
+//  Created by Yunus Emre Koca on 22.08.2022.
+//
+
+import UIKit
+
+class NewsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        title = "Haberler"
+    }
+}
